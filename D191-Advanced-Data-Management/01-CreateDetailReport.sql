0bca60ed9403b4099c41cf4e8bd9c75e94383bf4
scatteredsky <67299222+scatteredsky@users.noreply.github.com>
2022-07-03 16:06:01 -0500

    update detailed report table

--- D191-Advanced-Data-Management/01-CreateDetailReport.sql
+++ D191-Advanced-Data-Management/01-CreateDetailReport.sql
@@ -1,6 +1,6 @@
 --Creates detail report table for business question
 DROP TABLE IF EXISTS movies_detailed_report;
-CREATE TABLE IF NOT EXISTS detailed_report (
+CREATE TABLE IF NOT EXISTS movies_detailed_report (
 title VARCHAR(100),
 runtime VARCHAR(10),
 rentals INTEGER,
